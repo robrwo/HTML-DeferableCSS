c40a47b2ad8a5aade9b13797be9874497f8fb258
Robert Rothenberg <rrwo@cpan.org>
2020-02-20 16:53:33 +0000

    Bump VERSION

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ HTML::DeferableCSS - Simplify management of stylesheets in your HTML
 
 # VERSION
 
-version v0.1.3
+version v0.2.0
 
 # SYNOPSIS
 
--- lib/HTML/DeferableCSS.pm
+++ lib/HTML/DeferableCSS.pm
@@ -20,7 +20,7 @@ use Types::Standard qw/ Bool CodeRef HashRef Maybe Tuple /;
 
 use namespace::autoclean;
 
-our $VERSION = 'v0.1.3';
+our $VERSION = 'v0.2.0';
 
 =head1 SYNOPSIS
 
